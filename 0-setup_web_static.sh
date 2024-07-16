227d6a9e3628a1782ac2206d394c16f204999e31
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-16 16:00:12 +0100

    Resolved issue in 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,33 +13,33 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/current
 
-echo -e "
-<html>
+echo -e "<html>
     <head>
     </head>
     <body>
         <h1>Joseph&#39;s web page</h1>
     </body>
-</html>
-" > sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+for file in /data/web_static/current/*
+do
+    if [ -L "$file" ]
+    then
+	    rm /data/web_static/current/*
+    fi
+done
+
+sudo ln -s /data/web_static/releases/test/* /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data
 
 # Update the Nginx configuration
-echo -e "
-server {
-    listen 80;
-    listen [::]:80;
+replace_string="server_name devjoseph.tech www.devjoseph.tech;\n\tlocation \
+	/hbnb_static {\n\t\talias /data/web_static/current/;\
+	\n\t\tindex index.html;\n\t}"
 
-    server_name devjoseph.tech www.devjoseph.tech;
-    
-    location /hbnb_static {
-        alias /data/web_static/curent/;
-    }
-" > sudo tee /etc/nginx/sites-available/hbnb_static
+sudo sed -i "s|server_name _;|${replace_string}|" /etc/nginx/sites-enabled/default
 
-sudo ln -s /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/hbnb_static
+# Restart nginx server
 
 sudo service nginx restart
