e2e91dc9f130e75652cedca507441bb420c3bda6
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-17 13:26:48 +0100

    Added do_deploy function that distributes an archive to my web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This script distributes an archive to my web servers"""
+from fabric.api import run, put, env
+import os
+
+
+env.use_ssh_config = True
+env.key_filename = '~/.ssh/school'
+env.hosts = ['54.152.97.27', '52.91.127.178']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """This function distributes an archive to my web servers"""
+    # Check if archive file exists
+    if not os.path.exists(f"./{archive_path}"):
+        return False
+
+    # Upload archive file on server and uncompress it
+    put(f'{archive_path}', '/tmp/', True)
+    archive_name = archive_path.split('/')[-1]
+    archive_name = archive_name.split('.')[0]
+
+    run(f"mkdir -p /data/web_static/releases/{archive_name}")
+    run(f"tar -xvzf /tmp/{archive_name}.tgz -C\
+         /data/web_static/releases/{archive_name}")
+
+    # Delete archive from server and create new symbolic link to new release
+    run(f"rm -r /tmp/{archive_name}.tgz", True)
+
+    run("stat /data/web_static/current > /dev/null 2>&1 &&\
+        [ -L /data/web_static/current ] && rm /data/web_static/current\
+        || echo ''")
+    run(f"ln -s /data/web_static/releases/{archive_name}/web_static\
+         /data/web_static/current")
